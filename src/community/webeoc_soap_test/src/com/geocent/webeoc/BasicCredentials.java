69d6d2d0a96236654eae15fd9b2e1fab7e8c8f01
Stephen Mann <thanthese@gmail.com>
2012-05-07 09:51:13 -0500

    commited webeoc web service test

---
+++ src/community/webeoc_soap_test/src/com/geocent/webeoc/BasicCredentials.java
@@ -0,0 +1,39 @@
+package com.geocent.webeoc;
+
+import com.esi911.webeoc7.api._1.WebEOCCredentials;
+
+/**
+ * The {@link WebEOCCredentials} object has a pretty messed up idea of personal
+ * credentials. This object contains the more standard items, and lets you
+ * easily build a {@link WebEOCCredentials} object from there.
+ */
+public class BasicCredentials {
+	private String username;
+	private String password;
+	private String position;
+	
+	public BasicCredentials(String username, String password, String position) {
+		this.username = username;
+		this.password = password;
+		this.position = position;
+	}
+	
+	public WebEOCCredentials getWebEOCCredentialsNoIncident() {
+		WebEOCCredentials creds = new WebEOCCredentials();
+		creds.setUsername(username);
+		creds.setPassword(password);
+		creds.setPosition(position);
+		return creds;
+	}
+	
+	public WebEOCCredentials getWebEOCCredentials(String incident) {
+		if( incident == null ) throw new IllegalArgumentException("Incident cannot be null");
+		
+		WebEOCCredentials creds = new WebEOCCredentials();
+		creds.setUsername(username);
+		creds.setPassword(password);
+		creds.setPosition(position);
+		creds.setIncident(incident);
+		return creds;
+	}
+}
