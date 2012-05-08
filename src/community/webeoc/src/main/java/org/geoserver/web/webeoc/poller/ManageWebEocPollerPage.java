c3dd08b59427290fb999f4b93ca4f897a05dc92e
Stephen Mann <thanthese@gmail.com>
2012-05-08 15:32:59 -0500

    Ugly as sin, but the poller screen technically works.
    Technically.  Barely.

--- src/community/webeoc/src/main/java/org/geoserver/web/webeoc/poller/ManageWebEocPollerPage.java
+++ src/community/webeoc/src/main/java/org/geoserver/web/webeoc/poller/ManageWebEocPollerPage.java
@@ -1,28 +1,32 @@
 package org.geoserver.web.webeoc.poller;
 
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.SubmitLink;
 import org.geoserver.web.GeoServerSecuredPage;
 import org.geoserver.webeoc.poller.Poller;
 
 public class ManageWebEocPollerPage extends GeoServerSecuredPage {
 
 	private final Form form;
-	
+
 	private static final long DEV_defaultInterval = 5000l;
 
 	public ManageWebEocPollerPage() {
 
-		add(form = new Form("form"));
-		
 		final Poller poller = Poller.getInstance();
 		final boolean isRunning = poller.isRunning();
 
-		@SuppressWarnings("serial")
-		SubmitLink saveLink = new SubmitLink(isRunning ? "Stop" : "Start", form) {
+		if (isRunning) {
+			add(new Label("msg", "Is running"));
+		} else {
+			add(new Label("msg", "Isn't running"));
+		}
+
+		add(form = new Form("form") {
+
 			@Override
-			public void onSubmit() {
-				if(isRunning) {
+			protected void onSubmit() {
+				if (isRunning) {
 					System.out.println("Stop this!");
 					poller.stop();
 				} else {
@@ -30,9 +34,7 @@ public class ManageWebEocPollerPage extends GeoServerSecuredPage {
 					poller.start(DEV_defaultInterval);
 				}
 			}
-		};
-		form.add(saveLink);
-		form.setDefaultButton(saveLink);
+		});
 
 	}
 }
--- src/community/webeoc/src/main/java/org/geoserver/webeoc/poller/Poller.java
+++ src/community/webeoc/src/main/java/org/geoserver/webeoc/poller/Poller.java
@@ -1,10 +1,13 @@
 package org.geoserver.webeoc.poller;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class Poller {
+public class Poller implements Serializable {
+	
+	private static final long serialVersionUID = 8373003450818540645L;
 	
 	// singleton pattern
 	private static final Poller INSTANCE = new Poller();
@@ -40,7 +43,7 @@ public class Poller {
 
 	/* inner classes */
 
-	public class PollerTask extends TimerTask {
+	public static class PollerTask extends TimerTask {
 		@Override
 		public void run() {
 			// TODO: the polling logic
