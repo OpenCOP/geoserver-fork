5818d2db81f20a0eeb248ef3ab0586083e0124c3
unknown <pcoleman@pcoleman-PC.(none)>
2012-05-15 14:52:55 -0500

    Poller enable status now saved, poller interval time now in minutes, along with some rudamentary checking (can't be less than 5 minutes)

--- src/community/webeoc/src/main/java/org/geoserver/web/webeoc/poller/ManageWebEocPollerPage.java
+++ src/community/webeoc/src/main/java/org/geoserver/web/webeoc/poller/ManageWebEocPollerPage.java
@@ -26,13 +26,14 @@ import org.geoserver.webeoc.Poller;
 import org.geoserver.webeoc.WebEocDao;
 
 public class ManageWebEocPollerPage extends GeoServerSecuredPage {
-	
+
     private Form form;
     private Form resetLayerForm;
     private DropDownChoice layerDropDown;
     private boolean pollerEnabledModel;
     private String pollerIntervalModel;
     public String selectedResetLayer = "NOTHING";
+    private static final long DEFAULT_POLLING_INTERVAL_MILLISECONDS = 5 * 60 * 1000L;
 
     public ManageWebEocPollerPage() {
 
@@ -41,7 +42,22 @@ public class ManageWebEocPollerPage extends GeoServerSecuredPage {
         // Get the poller interval from global settings, gotta be null-safe!
         Serializable temp = GeoServerApplication.get().getGeoServer().getGlobal().getMetadata().get(WebEOCConstants.WEBEOC_POLLING_INTERVAL_KEY);
         if (null != temp) {
-            pollerIntervalModel = temp.toString();
+            /*
+             * Convert the value back into minutes for the user
+             */
+            try {
+                long tempLong = Long.parseLong(temp.toString());
+                float tempFloat = tempLong / (60 * 1000f);
+                pollerIntervalModel = Float.toString(tempFloat);
+            } catch (Exception e) {
+                e.printStackTrace();
+                pollerIntervalModel = Long.toString(DEFAULT_POLLING_INTERVAL_MILLISECONDS / (60 * 1000));
+            }
+        }
+
+        Serializable pollerEnabledValue = GeoServerApplication.get().getGeoServer().getGlobal().getMetadata().get(WebEOCConstants.WEBEOC_POLLING_ENABLED_KEY);
+        if (pollerEnabledValue != null) {
+            pollerEnabledModel = Boolean.parseBoolean(pollerEnabledValue.toString());
         }
 
         // Create the form
@@ -52,6 +68,8 @@ public class ManageWebEocPollerPage extends GeoServerSecuredPage {
                 new PropertyModel<String>(this, "pollerIntervalModel"));
         pollingInterval.setOutputMarkupId(true);
         form.add(pollingInterval);
+
+
 
         // Create the checkbox for enabling/disabling the poller
         final CheckBox pollerEnabled = new CheckBox("pollerEnabled",
@@ -73,28 +91,66 @@ public class ManageWebEocPollerPage extends GeoServerSecuredPage {
 
                 // Gotta be null-safe!
                 Serializable temp = metadata.get(WebEOCConstants.WEBEOC_POLLING_INTERVAL_KEY);
-                String oldInterval = "";
+                Long oldInterval = DEFAULT_POLLING_INTERVAL_MILLISECONDS; /*
+                 * initialize oldInterval to 5 minutes worth of milliseconds
+                 * incase it isn't set
+                 */
                 if (null != temp) {
-                    oldInterval = temp.toString();
+                    oldInterval = Long.parseLong(temp.toString());
+                } else {
+                    /*
+                     * if it was null that means there wasn't anything set for
+                     * the polling interval, that is silly! we'll put one in
+                     * right now!
+                     */
+                    metadata.put(WebEOCConstants.WEBEOC_POLLING_INTERVAL_KEY, DEFAULT_POLLING_INTERVAL_MILLISECONDS);
                 }
 
-                // Only update the poller interval if its different
-                // TODO: need further validation (number, greater than 0, etc.)
-                if (null != pollerIntervalModel && !pollerIntervalModel.equals(oldInterval)) {
-                    metadata.put(WebEOCConstants.WEBEOC_POLLING_INTERVAL_KEY, pollerIntervalModel);
+                float pollerIntervalFloat = -1;
+                try {
+                    pollerIntervalFloat = Float.parseFloat(pollerIntervalModel);
 
-                    // persist the global settings
-                    GeoServerApplication.get().getGeoServer().save(global);
+                    /*
+                     * Convert from minutes to milliseconds
+                     */
+                    pollerIntervalFloat = pollerIntervalFloat * 60 * 1000;
+
+                    /*
+                     * Check to make sure pollerIntervalModel is greater than 5
+                     * minutes, if it isn't, we will silently set it to 5
+                     * minutes
+                     */
+                    if (pollerIntervalFloat < DEFAULT_POLLING_INTERVAL_MILLISECONDS) {
+                        pollerIntervalFloat = DEFAULT_POLLING_INTERVAL_MILLISECONDS;
+                    }
+
+                    metadata.put(WebEOCConstants.WEBEOC_POLLING_INTERVAL_KEY, Long.toString((long) Math.round(pollerIntervalFloat)));
+
+                } catch (NumberFormatException e) {
+                    System.out.println("User entered in an invalid polling interval, " + pollerIntervalModel + " " + e.getMessage());
                 }
+
 
                 // Change the poller's status based on user input
                 if (!pollerEnabledModel) {
                     System.out.println("Stop this!");
+                    metadata.put(WebEOCConstants.WEBEOC_POLLING_ENABLED_KEY, false);
                     Poller.getInstance().stop();
                 } else {
                     System.out.println("Start this!");
-                    Poller.getInstance().start(Long.valueOf(pollerIntervalModel));
+                    metadata.put(WebEOCConstants.WEBEOC_POLLING_ENABLED_KEY, true);
+                    /*
+                     * If the pollerIntervalLong was set incorrectly by the user
+                     * (and is therefore still set to -1) we will use the
+                     * previously recorded value, otherwise we will use the new
+                     * value)
+                     */
+                    Poller.getInstance().start(pollerIntervalFloat == -1 ? oldInterval : (long) Math.round(pollerIntervalFloat));
                 }
+
+                // persist the global settings
+                GeoServerApplication.get().getGeoServer().save(global);
+
 
                 // if you don't do this, the page won't refresh right
                 setResponsePage(new ManageWebEocPollerPage());
@@ -143,8 +199,9 @@ public class ManageWebEocPollerPage extends GeoServerSecuredPage {
             @Override
             public void onSubmit() {
                 String val = selectedResetLayer;
-                if(val == null)
+                if (val == null) {
                     return;
+                }
                 Catalog catalog = GeoServerApplication.get().getCatalog();
                 LayerInfo layer = catalog.getLayerByName(val);
                 if (layer == null) {
--- src/community/webeoc/src/main/java/org/geoserver/webeoc/Poller.java
+++ src/community/webeoc/src/main/java/org/geoserver/webeoc/Poller.java
@@ -5,95 +5,115 @@ import java.util.TimerTask;
 
 import org.geoserver.catalog.Catalog;
 import org.geoserver.config.GeoServer;
+import org.geoserver.web.GeoServerApplication;
 import org.geoserver.web.data.webeoc.WebEOCConstants;
 
 public class Poller {
 
-	private Timer timer = null;
-	private long intervalMs;
+    private Timer timer = null;
+    private long intervalMs;
 
-	/*
-	 * This class has some round-about hacks to it. There were two problems to
-	 * solve.
-	 * 
-	 * 1. The poller needs an instance of catalog. This can only reasonably come
-	 * from a Spring injection, which necessitates a public constructor and a
-	 * catalog instance variable.
-	 * 
-	 * 2. Pages need a way to access this class. Spring dependency injection
-	 * isn't going to work because of limitations in GeoserverBasePage. Hence,
-	 * we need to save a static instance of this class.
-	 */
-	private final Catalog catalog;
-	private static Poller instance = null;
-	public static Poller getInstance() { return instance; }
+    /*
+     * This class has some round-about hacks to it. There were two problems to
+     * solve.
+     *
+     * 1. The poller needs an instance of catalog. This can only reasonably come
+     * from a Spring injection, which necessitates a public constructor and a
+     * catalog instance variable.
+     *
+     * 2. Pages need a way to access this class. Spring dependency injection
+     * isn't going to work because of limitations in GeoserverBasePage. Hence,
+     * we need to save a static instance of this class.
+     */
+    private final Catalog catalog;
+    private static Poller instance = null;
 
-	/**
-	 * It is available for Spring's use only. Use `getInstance()` instead.
-	 * @throws Exception 
-	 */
-	public Poller(GeoServer geoserver) throws Exception {
-		if(instance != null) { 
-			throw new Exception("Poller can only be instantiated once."); 
-		}
-		
-		instance = this;
-		this.catalog = geoserver.getCatalog();
-		
-		int pollingInterval = getPollingInterval(geoserver);
-		System.out.printf("The polling interval is %s\n", pollingInterval);
-		
-		start(pollingInterval);   
-	}
-	
-	/**
-	 * Retrieve the polling interval from geoserver's xml files.
-	 */
-	private int getPollingInterval(GeoServer geoserver) {
-		try {
-			String pollingIntervalStr = geoserver
-					.getGlobal()
-					.getMetadata()
-					.get(WebEOCConstants.WEBEOC_POLLING_INTERVAL_KEY)
-					.toString();
-			return Integer.valueOf(pollingIntervalStr);
-		} catch (Exception e) {  // if anything at all goes wrong, use default
-			System.out.println("ERROR: failed to get polling interval from file, using default.");
-			e.printStackTrace();
-			return WebEOCConstants.WEBEOC_POLLING_INTERVAL_DEFAULT;
-		}
-	}
-	
-	/**
-	 * Starts the poller. If the poller is already running, changes the polling
-	 * interval and forces the poller to poll immediately.
-	 */
-	public void start(long intervalMs) {
-		stop();
+    public static Poller getInstance() {
+        return instance;
+    }
 
-		this.intervalMs = intervalMs;
-		timer = new Timer(true);
-		timer.scheduleAtFixedRate(new TimerTask() {
-			@Override
-			public void run() {
-				UpdateTask.updateWebEocTables(catalog);
-			}
-		}, 0l, intervalMs);
-	}
+    /**
+     * It is available for Spring's use only. Use `getInstance()` instead.
+     *
+     * @throws Exception
+     */
+    public Poller(GeoServer geoserver) throws Exception {
+        if (instance != null) {
+            throw new Exception("Poller can only be instantiated once.");
+        }
 
-	public void stop() {
-		if (timer == null) return;
+        instance = this;
+        this.catalog = geoserver.getCatalog();
 
-		timer.cancel();
-		timer = null;
-	}
+        int pollingInterval = getPollingInterval(geoserver);
+        System.out.printf("The polling interval is %s\n", pollingInterval);
+        /*
+         * This start needs to be wrapped in some sort of if statement depending
+         * on if the poller is enabled or not, check the saved settings for this
+         * information
+         */
+        Object o = geoserver.getGlobal().getMetadata().get(WebEOCConstants.WEBEOC_POLLING_ENABLED_KEY);
+        if (o == null) {
+            /*
+             * if it isn't set we will assume its true and put it in there
+             */
+            start(pollingInterval);
+            geoserver.getGlobal().getMetadata().put(WebEOCConstants.WEBEOC_POLLING_ENABLED_KEY, true);
+        } else {
+            Boolean pollDat = Boolean.parseBoolean(o.toString());
+            if (pollDat) {
+                start(pollingInterval);
+            }
+        }
+    }
 
-	public void pollNow() {
-		stop();
-		start(intervalMs);
-	}
+    /**
+     * Retrieve the polling interval from geoserver's xml files.
+     */
+    private int getPollingInterval(GeoServer geoserver) {
+        try {
+            String pollingIntervalStr = geoserver.getGlobal().getMetadata().get(WebEOCConstants.WEBEOC_POLLING_INTERVAL_KEY).toString();
+            return Integer.valueOf(pollingIntervalStr);
+        } catch (Exception e) {  // if anything at all goes wrong, use default
+            System.out.println("ERROR: failed to get polling interval from file, using default.");
+            e.printStackTrace();
+            return WebEOCConstants.WEBEOC_POLLING_INTERVAL_DEFAULT;
+        }
+    }
 
-	public boolean isRunning() {
-		return timer != null;
-	}
+    /**
+     * Starts the poller. If the poller is already running, changes the polling
+     * interval and forces the poller to poll immediately.
+     */
+    public void start(long intervalMs) {
+        stop();
+
+        this.intervalMs = intervalMs;
+        timer = new Timer(true);
+        timer.scheduleAtFixedRate(new TimerTask() {
+
+            @Override
+            public void run() {
+                UpdateTask.updateWebEocTables(catalog);
+            }
+        }, 0l, intervalMs);
+    }
+
+    public void stop() {
+        if (timer == null) {
+            return;
+        }
+
+        timer.cancel();
+        timer = null;
+    }
+
+    public void pollNow() {
+        stop();
+        start(intervalMs);
+    }
+
+    public boolean isRunning() {
+        return timer != null;
+    }
 }
